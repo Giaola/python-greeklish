e69e104d43dc99dd80cf48f7deec81d1ec846b47
Marsel Tzatzo <tmarselg@gmail.com>
2017-03-31 08:16:40 +0300

    Setup.py

---
+++ setup.py
@@ -0,0 +1,73 @@
+from setuptools import setup
+
+import re
+import os
+
+package = 'greeklish'
+
+requirements = [
+    'six',
+]
+test_requirements = [
+    'six',
+]
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+setup(
+    name='python-greeklish',
+    version='1.0.0',
+    url='https://github.com/Giaola/python-greeklish',
+    license='MIT',
+    description='A greek to greeklish converter.',
+    author='Marsel Tzatzo',
+    author_email='tmarselg@gmail.com',
+    packages=get_packages(package),
+    package_data=get_package_data(package),
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='greeklish.tests',
+    tests_require=test_requirements
+)
