5dc65bb6f975589b0571f46b2c8724c062fc8d30
Marsel Tzatzo <tmarselg@gmail.com>
2017-04-03 17:01:32 +0300

    Setup.py version 1.1.1

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def get_package_data(package):
 
 setup(
     name='python-greeklish',
-    version='1.1.0',
+    version='1.1.1',
     url='https://github.com/Giaola/python-greeklish',
     license='MIT',
     description='A greek to greeklish converter.',
