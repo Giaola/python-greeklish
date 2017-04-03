fdcaa81428a161b28303e531790e01e6bc3f75dd
Marsel Tzatzo <tmarselg@gmail.com>
2017-04-03 17:00:27 +0300

    Converter > Minor bug fix & print cleanup

--- greeklish/converter.py
+++ greeklish/converter.py
@@ -7,13 +7,13 @@ class Converter(object):
     GREEK_CHARACTERS = u"αβγδεζηθικλμνξοπρσςτυφχψω"
 
     def __init__(self, max_expansions, generate_greek_variants):
-        self.greek_words = []
         self.reverse_stemmer = ReverseStemmer()
         self.generator = Generator(max_expansions)
         self.generate_greek_variants = generate_greek_variants
 
 
     def convert(self, input_token):
+        self.greek_words = []
 
         if not self.is_greek_word(input_token):
             return None
@@ -27,9 +27,7 @@ class Converter(object):
 
 
     def is_greek_word(self, input_token):
-        print(self.GREEK_CHARACTERS)
         for char in input_token:
-            print(char)
             if char not in self.GREEK_CHARACTERS:
                 return False
 
