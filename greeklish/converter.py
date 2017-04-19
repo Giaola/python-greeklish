535ec1be2f219441488c6949ba068e2487b2c81b
Marsel Tzatzo <tmarselg@gmail.com>
2017-04-19 10:44:19 +0300

    Converter > convert_phrase functionality added

--- greeklish/converter.py
+++ greeklish/converter.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import itertools
+
 from .reverse_stemmer import ReverseStemmer
 from .generator import Generator
 
@@ -24,6 +26,45 @@ class Converter(object):
             self.greek_words.append(input_token)
 
         return self.generator.generate_greeklish_words(self.greek_words)
+
+
+    def convert_phrase(self, phrase):
+        """
+        Converts a phrase to greeklish and returns all possible combinations.
+        e.g. If it is a phrase with 3 words and 1st word generates 2 greeklish tokens,
+        2nd word 4 greeklish tokens and 3rd word 1 greeklish token,
+        then 2 * 4 * 1 greeklish phrases will be generated.
+
+        Using this with generate_greek_variants on will produce significantly larger lists of variations.
+
+        :param phrase:      Phrase to be converted. Should contain only greek not-accented words,
+                            and no punctuation.
+        :return:            Greeklish phrases generated.
+        """
+        self.phrase_words = phrase.split(sep=' ')
+
+        for word in self.phrase_words:
+            if not self.is_greek_word(word):
+                return None
+
+        self.converted_phrase_words = []
+        if self.generate_greek_variants:
+            for word in self.phrase_words:
+                converted_stemmed_words = []
+                stemmed_words = self.reverse_stemmer.generate_greek_variants(word)
+
+                for stemmed_word in stemmed_words:
+                    converted_stemmed_words.extend(self.generator.generate_greeklish_words(stemmed_word))
+
+                self.converted_phrase_words.append(converted_stemmed_words)
+
+        else:
+            for word in self.phrase_words:
+                self.converted_phrase_words.append(self.generator.generate_greeklish_words(word))
+
+        return [" ".join(tuple)
+                for tuple in list(itertools.product(*self.converted_phrase_words))]
+
 
 
     def is_greek_word(self, input_token):
